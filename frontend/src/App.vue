5d7fda6899ec0465c1981950d21834fcd2c62035
Rohit Kumar <rohitkumar.rk0600@gmail.com>
2025-06-01 17:33:14 +0530

    code

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <Navbar />
+  
+  <RouterView />
+</template>
+
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+
+const email = ref('');
+const password = ref('');
+
+console.log(email.value, password.value);
+
+</script>
+
+<template>
+    <h1>Welcome to Grocery Store</h1>
+    <h2>Login</h2>
+    <form class = 'container-fluid'>
+        <div class="mb-3">
+            <label for="email">Email</label>
+            <input type="text" v-model="email" id="email" required>
+        </div>
+
+        <p>{{ email }}</p>
+
+        <div class = "mb-3">
+            <label for="password">Password</label>
+            <input type="password" v-model="password" id="password" required>
+        </div>
+
+        {{password}}
+        <button type="submit">Login</button>
+    </form>
+    <RouterLink to='/register'>Create new User?</RouterLink>
+
+</template>
